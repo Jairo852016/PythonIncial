73e23c4ad179489482ff3a7ed44130ed8a2fe27f
Jairo Perez <jairo85cd@gmail.com>
2022-03-04 22:11:16 -0500

    Prime comitcon los ejercicios

---
+++ ejemplo1.py
@@ -0,0 +1,54 @@
+nombre="Jairo"
+print(nombre)
+nombre2="Maria jose"
+print(nombre2)
+print(nombre+nombre2)
+print(nombre *4)
+numero_decimal=3.4
+print(numero_decimal)
+es_estudiante=True
+print(es_estudiante)
+
+
+numero1= input("Escribe un numero: ")
+print(numero1)
+numero2=input("Escribe el numero2: ")
+print(numero2)
+print(numero1+numero2)
+numero1=int(numero1)
+numero2=int(numero2)
+print(numero1+numero2)
+numdec=4.5
+print(int(4.5))
+numdec=str(numdec)
+print(numdec)
+
+es_estudiante=True
+trabaja=False
+if es_estudiante and trabaja:
+    prirn(True)
+else:
+    print(False)
+
+
+nombre="facundo "
+print(nombre)
+print(nombre.upper())
+print(nombre.capitalize())
+nombre=nombre.capitalize()
+print(nombre.strip())
+print(nombre.lower())
+nombre=nombre.replace('o','a')
+print(nombre)
+print(nombre[0])
+print(len(nombre))
+nombre="Francisco"
+print(nombre)
+print(nombre[0:3])
+print(nombre[:3])
+print(nombre[3:])
+print(nombre[1:7])
+print(nombre[1:7:2])
+print(nombre[::])
+print(nombre[1::3])
+print(nombre[::-1])
